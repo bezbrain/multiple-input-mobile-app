14c39a8cd54b86e4f68d76e9b4c9901594d6f1cc
bezbrain <belloquadriolawale@gmail.com>
2024-04-25 15:36:35 +0100

    Add touchwithoutfeedback component to close keyboard

--- App.tsx
+++ App.tsx
@@ -1,15 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Keyboard,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import Form from "./components/form";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <StatusBar style="auto" /> */}
-      <SafeAreaView>
-        <Form />
-      </SafeAreaView>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        {/* <StatusBar style="auto" /> */}
+        <SafeAreaView>
+          <Form />
+        </SafeAreaView>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
